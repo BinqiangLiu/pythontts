d42e0b88091bfdac73c977c1ac65746a7cdf029d
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-21 16:25:45 +0800

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,11 +1,10 @@
 # https://medium.com/@pavlo_sydorenko/add-text-to-speech-to-your-web-app-with-5-lines-of-python-code-8c4707f2dc93
 
 import streamlit as st
-#from gtts import gTTS
-#from io import BytesIO
 
-#sound_file = BytesIO()
-#tts = gTTS('AStreamlit is a free and open-source framework to rapidly build and share beautiful machine learning and data science web apps.', lang='en')
-#tts.write_to_fp(sound_file)
+mp3_file_path = "./PythonTTS.mp3"
 
-st.audio(PythonTTS.mp3)
+with open(mp3_file_path, "rb") as file:
+    audio_file = file.read()
+
+st.audio(audio_file)
