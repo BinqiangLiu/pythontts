74059972ef4c7f731554d8ae470b120f1df72f1f
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-24 10:25:33 +0800

    Update tts.py

--- tts.py
+++ tts.py
@@ -10,7 +10,7 @@ mp3_file_path = "PythonTTS.mp3"
 with open(mp3_file_path, "rb") as file:
     audio_file = file.read()
 st.audio(audio_file)
-
+st.write("---")
 #但是，这个app是要测试tts即text to speech而不只是st.audio的音频播放功能
 #下面用Google的gtts测试
 st.write("Google gtts Function Test")
@@ -20,7 +20,7 @@ from io import BytesIO
 
 mp3_fp = BytesIO()
 text_input = st.text_input("请在下方文本框中输入要转换为语音的文本", value="请输入要转换为语音的文本")
-tts = gTTS(text_input, lang='zh-TW')
+tts = gTTS(text_input, lang='zh-CN')
 tts.write_to_fp(mp3_fp)
 st.write('mp3_fp')
 # Playing sound directly
@@ -33,6 +33,7 @@ st.write("---")
 import streamlit as st
 st.audio('ttsAudio.mp3')
 
+st.write("---")
 #新增一个按钮测试直接播放语音文件
 st.write("再测试增加一个按钮控制播放指定的音频文件（该文件同样是预先上传到与Streamlit app相同的根目录之下的某个位置（可以和app主程序位置不同）")
 if st.button("直接播放上传语音文件"):
